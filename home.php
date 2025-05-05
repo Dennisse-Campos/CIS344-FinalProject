d1a7683ecf07c03c7897011ee61c9a3dbae31293
Dennisse-Campos <dennisse.campos@lc.cuny.edu>
2025-05-05 12:11:19 -0400

    Update home.php

--- home.php
+++ home.php
@@ -12,6 +12,7 @@
         <a href="viewPrescriptions.php" class="nav-link">View Prescriptions</a> 
         <a href="addMedication.php" class="nav-link">Add Medication</a>
         <a href="addInventory.php" class="nav-link">Add to Inventory</a>
+         <a href="viewInventory.php" class="nav-ink">View Inventory</a>
         <a href="logout.php">Logout</a>  <!-- Temp log out button-->
         
     </nav>
